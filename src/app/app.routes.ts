16de7c4f0b5bc01df8ae1e6b1c6c410999b4e7e6
Emma Heikkinen <emhe2504@student.miun.se>
2026-04-22 09:07:44 +0200

    Startade upp sidan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Calculator } from './pages/calculator/calculator';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: Home },
+    { path: "calculator", component: Calculator },
+    { path: "about", component: About }
+];
---
+++ src/app/pages/mainheader/mainheader.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-mainheader',
+  imports: [RouterLink],
+  templateUrl: './mainheader.html',
+  styleUrl: './mainheader.css',
+})
+export class Mainheader {}
