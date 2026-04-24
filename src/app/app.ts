a2c1a4786474547cd577d0921ccc25f3e51d73a1
Emma Heikkinen <emhe2504@student.miun.se>
2026-04-24 09:55:56 +0200

    Innehåll i about samt mainfooter skapad

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Mainheader } from './pages/mainheader/mainheader';
+import { Mainfooter } from './mainfooter/mainfooter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Mainheader],
+  imports: [RouterOutlet, Mainheader, Mainfooter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
